4e0fd9645ab389bf70b0f5fa3a12ab53e359f2ff
s:103 <mamingsheng103@yeah.net>
2024-01-15 14:43:09 +0800

    feat: add api about short link

---
+++ src/main/java/com/achobeta/domain/shortlink/controller/ShortLinkController.java
@@ -0,0 +1,53 @@
+package com.achobeta.domain.shortlink.controller;
+
+import com.achobeta.common.SystemJsonResponse;
+import com.achobeta.domain.shortlink.service.ShortLinkService;
+import com.achobeta.domain.shortlink.util.HttpUrlValidator;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/shortlink")
+public class ShortLinkController {
+
+    private final ShortLinkService shortLinkService;
+
+
+    /**
+     * 重定向短链接
+     * @param code 短链code
+     * @return 重定向到原链接
+     */
+    @GetMapping("/{code}")
+    public RedirectView getShortLink(@PathVariable("code")String code) {
+        String originUrl = shortLinkService.getOriginUrl(code);
+        return new RedirectView(originUrl);
+    }
+
+    /**
+     * 长转短
+     * @param request 用来获取host
+     * @param url 原链接
+     * @return 短链接
+     */
+    @PostMapping("/trans")
+    public SystemJsonResponse transferAndSaveShortLink(HttpServletRequest request, @RequestParam("url")String url) {
+        //验证url
+        if(!HttpUrlValidator.isHttpUrl(url) || !HttpUrlValidator.isUrlAccessible(url)) {
+            throw new IllegalArgumentException("url is not valid");
+        }
+        // 拼接出基础的url
+        String baseUrl = "http://" + request.getHeader("host") + "/shortlink/";
+        // 转化
+        String shortLinkURL = shortLinkService.transShortLinkURL(baseUrl, url);
+        return SystemJsonResponse.SYSTEM_SUCCESS(shortLinkURL);
+    }
+
+
+}
