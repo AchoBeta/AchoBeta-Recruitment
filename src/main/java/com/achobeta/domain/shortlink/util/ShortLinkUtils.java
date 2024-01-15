436badc05713a491f0752d91d6a53bccc88d3013
s:103 <mamingsheng103@yeah.net>
2024-01-15 14:51:59 +0800

    feat: add generator about short code

---
+++ src/main/java/com/achobeta/domain/shortlink/util/ShortLinkUtils.java
@@ -0,0 +1,47 @@
+package com.achobeta.domain.shortlink.util;
+
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.UUID;
+
+public class ShortLinkUtils {
+
+    private static final String CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+    private static final int LINK_LENGTH = 6;
+
+    public static final String LINK = "link:";
+
+
+    // 获取盐值
+    public static String getSalt() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    // md5加密
+    public static String md5(String normal) {
+        return DigestUtils.md5Hex(normal);
+    }
+
+    public static String subCodeByString(String str) {
+        int gap = str.length() / LINK_LENGTH;//取值间隔
+        if(gap < 4) {
+            return null;
+        }
+        StringBuilder subCode = new StringBuilder();
+        for (int i = 0; i < LINK_LENGTH; i++) {
+            int index = Integer.parseInt(str.substring(i * gap, i * gap + 4), 16);//提取十六进制数
+            subCode.append(CHARSET.charAt(index % CHARSET.length()));//对应到Base64字典的某个Base64字符
+        }
+        return subCode.toString();
+    }
+
+    public static String getShortCodeByURL(String url) {
+        String hash = md5(url + getSalt());
+        return subCodeByString(hash);
+    }
+
+
+
+
+}
