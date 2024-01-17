2b9e5d74d43e763df64917a59529dc0ec580eada
s:103 <mamingsheng103@yeah.net>
2024-01-18 02:08:32 +0800

    feat: add some methods

--- src/main/java/com/achobeta/domain/users/util/IdentifyingCodeValidator.java
+++ src/main/java/com/achobeta/domain/users/util/IdentifyingCodeValidator.java
@@ -1,14 +1,14 @@
 package com.achobeta.domain.users.util;
 
-import java.util.Random;
+import cn.hutool.core.util.RandomUtil;
+
+import java.util.Map;
 
 public class IdentifyingCodeValidator {
 
     public static final String IDENTIFYING_CODE_PURPOSE = "验证用户身份";
 
     public static final int IDENTIFYING_CODE_SIZE = 6; // 验证码长度
-
-    public static final int IDENTIFYING_CODE_RADIX = 10; // 验证码每位的取值范围，也可以说是进制
 
     public static final String IDENTIFYING_CODE = "IDENTIFYING_CODE";
 
@@ -17,13 +17,14 @@ public class IdentifyingCodeValidator {
     public static final String REDIS_EMAIL_IDENTIFYING_CODE = "REDIS_EMAIL_IDENTIFYING_CODE_";
 
     public static String getIdentifyingCode() {
-        Random random = new Random();
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < IDENTIFYING_CODE_SIZE; i++) {
-            int number = random.nextInt(IDENTIFYING_CODE_RADIX);
-            builder.append(Integer.toString(number, IDENTIFYING_CODE_RADIX));
-        }
-        return builder.toString();
+        return RandomUtil.randomNumbers(IDENTIFYING_CODE_SIZE);
+    }
+
+    public static boolean isAllowedToSend(Map<String, Object> data, long intervalLimit, long timeout) {
+        long nowTime = System.currentTimeMillis(); // 当前时间
+        long deadline = (long)data.get(IDENTIFYING_DEADLINE); // 截止时间
+        long nextAllowedTime = deadline - timeout + intervalLimit; // 下个可以再次发送的点
+        return nowTime > nextAllowedTime;
     }
 
 }
