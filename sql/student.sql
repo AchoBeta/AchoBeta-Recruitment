198c3101fe611f18d215613377665c9c9abb323e
yuan <yuan.com>
2024-01-17 14:51:10 +0800

    fix:supplement volume comment on sql#12

--- sql/student.sql
+++ sql/student.sql
@@ -37,7 +37,23 @@ CREATE TABLE `student`  (
   `awards` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '获奖经历',
   `image` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '照片',
   `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '备注',
-  `status_id` int NOT NULL DEFAULT 1 COMMENT '简历状态',
+  `status_id` int NOT NULL DEFAULT 1 COMMENT '范围：0~14，简历状态{
+- 0-草稿
+- 1-待筛选
+- 2-筛选不通过
+- 3-待安排初试
+- 4-待初试
+- 5-初试不通过
+- 6-初试通过（仅当初试为最后一个流程时显示）
+- 7-待复试
+- 8-待安排复试
+- 9-复试通过（仅当复试为最后一个流程时显示）
+- 10-待终试
+- 11-待安排终试
+- 12-终试通过（仅当复试为最后一个流程时显示）
+- 13-待处理（反馈异常/或管理员主动设置为该状态）
+- 14-挂起（管理员可以主动设置该状态）
+}',
   `submit_count` int NOT NULL DEFAULT 0 COMMENT '提交次数',
   `batch` int NOT NULL DEFAULT 1 COMMENT 'ab版本',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
