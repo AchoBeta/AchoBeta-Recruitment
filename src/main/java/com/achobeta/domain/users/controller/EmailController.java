93cf8e56996d4e3dc1eed9bb97c64a7b24c6d01e
s:103 <mamingsheng103@yeah.net>
2024-01-18 16:56:19 +0800

    feat: check email pattern because the annotation is invalid

--- src/main/java/com/achobeta/domain/users/controller/EmailController.java
+++ src/main/java/com/achobeta/domain/users/controller/EmailController.java
@@ -1,9 +1,11 @@
 package com.achobeta.domain.users.controller;
 
 import com.achobeta.common.SystemJsonResponse;
+import com.achobeta.common.constants.GlobalServiceStatusCode;
+import com.achobeta.domain.email.component.EmailValidator;
 import com.achobeta.domain.users.service.EmailService;
 import com.achobeta.domain.users.util.IdentifyingCodeValidator;
-import jakarta.validation.constraints.Email;
+import com.achobeta.exception.GlobalServiceException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,8 +26,12 @@ public class EmailController {
      * @return
      */
     @PostMapping("/check")
-    public SystemJsonResponse emailIdentityCheck(@RequestParam("email") @Email String email) {
-        // 获得随机数
+    public SystemJsonResponse emailIdentityCheck(@RequestParam("email") @NonNull String email) {
+        // 判断 email 格式
+        if(!EmailValidator.isEmailAccessible(email)) {
+            throw new GlobalServiceException(GlobalServiceStatusCode.EMAIL_PATTERN_ERROR);
+        }
+        // 获得随机验证码
         String code = IdentifyingCodeValidator.getIdentifyingCode();
         emailService.sendIdentifyingCode(email, code);
         // 能到这一步就成功了
@@ -34,8 +40,13 @@ public class EmailController {
     }
 
     @PostMapping("/check/{code}")
-    public SystemJsonResponse checkCode(@RequestParam("email") @Email String email,
+    public SystemJsonResponse checkCode(@RequestParam("email") @NonNull String email,
                                         @PathVariable("code") @NonNull String code) {
+        // 判断 email 格式
+        if(!EmailValidator.isEmailAccessible(email)) {
+            throw new GlobalServiceException(GlobalServiceStatusCode.EMAIL_PATTERN_ERROR);
+        }
+        // 验证
         emailService.checkIdentifyingCode(email, code);
         // 成功
         log.info("email:{}, 验证码:{} 验证成功", email, code);
