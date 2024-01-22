13b839d27eb8f29468db100e9ecdac01d6de5697
BanTanger <1290288968@qq.com>
2024-01-22 14:02:48 +0800

    feat: add user.sql and format all sql file

---
+++ sql/user.sql
@@ -0,0 +1,31 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `uid`          bigint(20)   NOT NULL AUTO_INCREMENT COMMENT '用户唯一ID',
+    `user_name`    varchar(10)  NOT NULL DEFAULT '' COMMENT '姓名',
+    `email`        varchar(50)  NOT NULL DEFAULT '' COMMENT '邮箱',
+    `phone_number` varchar(11)  NOT NULL DEFAULT '' COMMENT '手机号码',
+    `password`     varchar(100) NOT NULL DEFAULT '' COMMENT '密码',
+    `user_type`    int          NOT NULL DEFAULT 1 COMMENT '用户类型：1.普通用户 2. 管理员',
+    `uuid`         long         NOT NULL DEFAULT 0 COMMENT '防重 id, 结合 uid ',
+    `create_time`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+    `version`      int          NOT NULL DEFAULT 0 COMMENT '乐观锁',
+    `is_deleted`   tinyint      NOT NULL DEFAULT 0 COMMENT '伪删除标记',
+    PRIMARY KEY (`uid`),
+    UNIQUE INDEX `uni_email` (`email`),
+    unique index `uni_phone` (`phone_number`),
+    unique index `uni_name` (`user_name`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 10000
+  CHARACTER SET = utf8
+  COLLATE = utf8_bin COMMENT = '用户基本信息表'
+  ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
