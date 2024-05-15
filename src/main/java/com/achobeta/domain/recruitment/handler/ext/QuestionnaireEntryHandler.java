096b47df12e92d25b78c41a7ac0e7e6f5187e039
s:103 <2040484356@qq.com>
2024-05-15 22:46:53 +0800

    feat: 提供“从题单中移除一道题”、“删除一份题单”两个事件，针对招新活动功能模块，存在的需要处理的事件

---
+++ src/main/java/com/achobeta/domain/recruitment/handler/ext/QuestionnaireEntryHandler.java
@@ -0,0 +1,52 @@
+package com.achobeta.domain.recruitment.handler.ext;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.achobeta.domain.paper.handler.RemoveQuestionHandler;
+import com.achobeta.domain.paper.model.entity.QuestionEntry;
+import com.achobeta.domain.recruitment.model.entity.QuestionnaireEntry;
+import com.achobeta.domain.recruitment.model.entity.RecruitmentActivity;
+import com.achobeta.domain.recruitment.service.QuestionnaireService;
+import com.achobeta.domain.recruitment.service.RecruitmentActivityService;
+import com.baomidou.mybatisplus.extension.toolkit.Db;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-05-15
+ * Time: 12:41
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class QuestionnaireEntryHandler extends RemoveQuestionHandler {
+
+    private final RecruitmentActivityService recruitmentActivityService;
+
+    private final QuestionnaireService questionnaireService;
+
+    @Override
+    public void handle(Long paperId, Long questionId) {
+        // 找到所有以 paperId 为问卷模板的招新活动的问卷 id 集合
+        List<Long> questionnaireIds = questionnaireService.getQuestionnaireIdsByPaperId(paperId);
+        if(CollectionUtil.isEmpty(questionnaireIds)) {
+            return;
+        }
+        // 删除对应的一行
+        Db.lambdaUpdate(QuestionnaireEntry.class)
+                .in(QuestionnaireEntry::getQuestionnaireId, questionnaireIds)
+                .eq(QuestionnaireEntry::getEntryId, questionId)
+                .remove();
+        // 执行下一个
+        super.doNextHandler(paperId, questionId);
+    }
+
+}
