3b57e5ee5b4e11b717454b4768ea7c5642670e46
s:103 <2040484356@qq.com>
2024-09-04 23:25:37 +0800

    fix: 添加一个注释

--- src/main/java/com/achobeta/domain/html/service/HtmlEngine.java
+++ src/main/java/com/achobeta/domain/html/service/HtmlEngine.java
@@ -85,6 +85,17 @@ public class HtmlEngine {
             return HtmlBuilder.this.clear().append(replacement);
         }
 
+        /**
+         * 通过替换唯一标识符，向原 html 插入一段 markdown 文本转换的 html
+         * @param uniqueSymbol 唯一标识符
+         * @param markdown 待插入的 markdown 文本
+         * @return HtmlBuilder.this
+         * 推荐用法：
+         * 假设邮件模板为 template，邮件模板数据为 data，data 里有一个字段 content，而 content 为 markdown 文本
+         * 1. 通过调用 htmlEngine.getUniqueSymbol()，获得一个唯一标识符 uniqueSymbol
+         * 2. content 先设置为 uniqueSymbol，然后调用 append，传入 template 和 data 合成初步的 html
+         * 3. 紧接着调用 replaceMarkdown，传入 uniqueSymbol 和原 markdown 文本，文本转换为 html 并替换 uniqueSymbol 的位置
+         */
         public HtmlBuilder replaceMarkdown(String uniqueSymbol, String markdown) {
             return HtmlBuilder.this.replace(uniqueSymbol, HtmlEngine.this.markdownToHtml(markdown));
         }
