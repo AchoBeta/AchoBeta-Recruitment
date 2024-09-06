f53e7aa1fdfee7e28dd481bf47579e201e41520c
s:103 <2040484356@qq.com>
2024-09-06 23:10:26 +0800

    fix: 调整 html 模板

--- src/main/java/com/achobeta/domain/html/service/HtmlEngine.java
+++ src/main/java/com/achobeta/domain/html/service/HtmlEngine.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
