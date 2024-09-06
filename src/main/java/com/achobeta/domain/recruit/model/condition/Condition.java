ec9439c8760063cfde0b8f19f399f7a3648cac41
s:103 <2040484356@qq.com>
2024-09-06 12:19:19 +0800

    fix: 调整一下代码

--- src/main/java/com/achobeta/domain/recruit/model/condition/Condition.java
+++ src/main/java/com/achobeta/domain/recruit/model/condition/Condition.java
@@ -15,13 +15,11 @@ import java.util.function.Predicate;
 public interface Condition {
 
     default Predicate<StuResume> skipAndIfNull(Condition condition) {
-        Predicate<StuResume> predicate = t -> Boolean.TRUE;
-        return predicate.and(stuResume -> Objects.isNull(condition) || condition.predicate().test(stuResume));
+        return stuResume -> Objects.isNull(condition) || condition.predicate().test(stuResume);
     }
 
     default Predicate<StuResume> skipOrIfNull(Condition condition) {
-        Predicate<StuResume> predicate = t -> Boolean.FALSE;
-        return predicate.and(stuResume -> Objects.nonNull(condition) && condition.predicate().test(stuResume));
+        return stuResume -> Objects.nonNull(condition) && condition.predicate().test(stuResume);
     }
 
     Predicate<StuResume> predicate();
