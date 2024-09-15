44b2dbe5da1a2b7dafe2902b63040065397c0843
s:103 <2040484356@qq.com>
2024-09-15 22:06:27 +0800

    feat: 增加记录用户简历状态过程的功能

---
+++ src/main/java/com/achobeta/domain/resumestate/model/dao/mapper/ResumeStatusProcessMapper.java
@@ -0,0 +1,18 @@
+package com.achobeta.domain.resumestate.model.dao.mapper;
+
+import com.achobeta.domain.resumestate.model.entity.ResumeStatusProcess;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 马拉圈
+* @description 针对表【resume_status_process.sql(招新简历状态过程表)】的数据库操作Mapper
+* @createDate 2024-09-15 20:22:47
+* @Entity com.achobeta.domain.resumestate.model.entity.ResumeStatusProcess
+*/
+public interface ResumeStatusProcessMapper extends BaseMapper<ResumeStatusProcess> {
+
+}
+
+
+
+
---
+++ src/main/java/com/achobeta/domain/resumestate/model/entity/ResumeStatusProcess.java
@@ -0,0 +1,37 @@
+package com.achobeta.domain.resumestate.model.entity;
+
+import com.achobeta.common.base.BaseIncrIDEntity;
+import com.achobeta.common.enums.ResumeEvent;
+import com.achobeta.common.enums.ResumeStatus;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 招新简历状态过程表
+ * @TableName resume_status_process.sql
+ */
+@TableName(value ="resume_status_process.sql")
+@Data
+public class ResumeStatusProcess extends BaseIncrIDEntity implements Serializable {
+
+    /**
+     * 简历 id
+     */
+    private Long resumeId;
+
+    /**
+     * 简历状态
+     */
+    private ResumeStatus resumeStatus;
+
+    /**
+     * 简历事件
+     */
+    private ResumeEvent resumeEvent;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
