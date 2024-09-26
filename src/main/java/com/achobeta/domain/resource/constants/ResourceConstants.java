e167014119d5564ef8ab70217a630c7596fb65cf
s:103 <2040484356@qq.com>
2024-09-26 18:49:35 +0800

    feat: 资源相关的常量

---
+++ src/main/java/com/achobeta/domain/resource/constants/ResourceConstants.java
@@ -0,0 +1,16 @@
+package com.achobeta.domain.resource.constants;
+
+import com.achobeta.domain.resource.enums.ResourceAccessLevel;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-09-26
+ * Time: 16:36
+ */
+public interface ResourceConstants {
+
+    ResourceAccessLevel DEFAULT_RESOURCE_ACCESS_LEVEL = ResourceAccessLevel.USER_ACCESS;
+
+}
