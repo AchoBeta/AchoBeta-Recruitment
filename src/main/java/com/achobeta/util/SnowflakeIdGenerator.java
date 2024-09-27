e330628899e9df97cd3b5ffb54ade1e4e6e5c01a
s:103 <2040484356@qq.com>
2024-09-27 20:58:49 +0800

    fix: 调整代码

--- src/main/java/com/achobeta/util/SnowflakeIdGenerator.java
+++ src/main/java/com/achobeta/util/SnowflakeIdGenerator.java
@@ -1,5 +1,7 @@
 package com.achobeta.util;
 
+import com.achobeta.exception.GlobalServiceException;
+
 public class SnowflakeIdGenerator {
 
     /**
@@ -7,53 +9,73 @@ public class SnowflakeIdGenerator {
      */
     private final long startTime = 1683803335498L;
 
-    /** 机器id所占的位数 */
+    /**
+     * 机器id所占的位数
+     */
     private final long workerIdBits = 3L;
 
-    /** 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数) */
+    /**
+     * 支持的最大机器id，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
+     */
     private final long maxWorkerId = -1L ^ (-1L << workerIdBits);
 
-    /** 序列在id中占的位数 */
+    /**
+     * 序列在id中占的位数
+     */
     private final long sequenceBits = 5L;
 
-    /** 机器ID向左移12位 */
+    /**
+     * 机器ID向左移12位
+     */
     private final long workerIdShift = sequenceBits;
 
-    /** 时间截向左移22位(10+12) */
+    /**
+     * 时间截向左移22位(10+12)
+     */
     private final long timestampLeftShift = sequenceBits + workerIdBits;
 
-    /** 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095) */
+    /**
+     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+     */
     private final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
-    /** 工作机器ID(0~1024) */
+    /**
+     * 工作机器ID(0~1024)
+     */
     private final long workerId;
 
-    /** 毫秒内序列(0~4095) */
+    /**
+     * 毫秒内序列(0~4095)
+     */
     private long sequence = 0L;
 
-    /** 上次生成ID的时间截 */
+    /**
+     * 上次生成ID的时间截
+     */
     private long lastTimestamp = -1L;
 
     /**
      * 构造函数
+     *
      * @param workerId 工作ID (0~1024)
      */
     public SnowflakeIdGenerator(long workerId) {
         if (workerId > maxWorkerId || workerId < 0) {
-            throw new IllegalArgumentException(String.format("workerId can't be greater than %d or less than 0", maxWorkerId));
+            throw new GlobalServiceException(String.format("workerId can't be greater than %d or less than 0", maxWorkerId));
         }
         this.workerId = workerId;
     }
 
     /**
      * 获得下一个ID (该方法是线程安全的)
+     *
      * @return SnowflakeId
      */
     public synchronized long nextId() {
         long timestamp = timeGen();
         //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
         if (timestamp < lastTimestamp) {
-            throw new RuntimeException(
+            throw new GlobalServiceException(
                     String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
         }
         //如果是同一时间生成的，则进行毫秒内序列
@@ -79,6 +101,7 @@ public class SnowflakeIdGenerator {
 
     /**
      * 阻塞到下一个毫秒，直到获得新的时间戳
+     *
      * @param lastTimestamp 上次生成ID的时间截
      * @return 当前时间戳
      */
@@ -92,6 +115,7 @@ public class SnowflakeIdGenerator {
 
     /**
      * 返回以毫秒为单位的当前时间
+     *
      * @return 当前时间(毫秒)
      */
     protected long timeGen() {
