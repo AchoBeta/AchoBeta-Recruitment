32355c9ec84a940ada87b1fc9ac1853067ff880c
s:103 <2040484356@qq.com>
2024-09-27 23:27:12 +0800

    fix: 删除无用依赖

--- src/main/java/com/achobeta/domain/login/service/strategy/PasswordLoginStrategy.java
+++ src/main/java/com/achobeta/domain/login/service/strategy/PasswordLoginStrategy.java
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import static com.achobeta.common.enums.GlobalServiceStatusCode.SYSTEM_SERVICE_FAIL;
