7dcf85729f095322ba99b1068cc4874cf2662cbf
s:103 <2040484356@qq.com>
2024-10-13 01:31:53 +0800

    fix: 调整代码

--- src/main/java/com/achobeta/domain/resource/service/ResourceService.java
+++ src/main/java/com/achobeta/domain/resource/service/ResourceService.java
@@ -16,9 +16,13 @@ import java.util.List;
  * Date: 2024-09-21
  * Time: 12:16
  * Description: 仅仅针对本服务内部的资源
+ * 本接口大部分方法都需要真实存在一次请求才能访问
  */
 public interface ResourceService {
 
+    /**
+     * 此方法可能需要真实存在一次请求才能调用（权限等级大于 FREE_ACCESS 时，此请求需要用到 HttpServletRequest，内部会尝试获取当前请求的 HttpServletRequest）
+     */
     DigitalResource analyzeCode(Long code, ResourceAccessLevel level);
 
     DigitalResource analyzeCode(Long code);
@@ -72,6 +76,7 @@ public interface ResourceService {
 
     /**
      * 压缩图片，同时更新对象上传服务器，以及数据库的记录
+     * 此方法不要求真实存在一次请求
      * @param code 资源码
      * @throws Exception 若不是图片会抛异常
      */
