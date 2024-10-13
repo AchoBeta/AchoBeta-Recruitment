cd69e7f70251d7c176852357f68ac46f40807c60
s:103 <2040484356@qq.com>
2024-10-13 23:56:36 +0800

    fix: 调整代码

--- src/main/java/com/achobeta/domain/resource/constants/ResourceConstants.java
+++ src/main/java/com/achobeta/domain/resource/constants/ResourceConstants.java
@@ -11,7 +11,6 @@ import com.achobeta.domain.resource.enums.ResourceAccessLevel;
  */
 public interface ResourceConstants {
 
-    // todo: 设置默认的权限等级
     ResourceAccessLevel DEFAULT_RESOURCE_ACCESS_LEVEL = ResourceAccessLevel.USER_ACCESS;
 
     ResourceAccessLevel DEFAULT_EXCEL_ACCESS_LEVEL = ResourceAccessLevel.FREE_ACCESS;
