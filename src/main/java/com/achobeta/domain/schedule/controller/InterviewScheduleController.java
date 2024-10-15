89de8ed17950d7610565fed9ea6f5bfbe6f4573d
s:103 <2040484356@qq.com>
2024-10-15 10:23:05 +0800

    fix: 调整代码

--- src/main/java/com/achobeta/domain/schedule/controller/InterviewScheduleController.java
+++ src/main/java/com/achobeta/domain/schedule/controller/InterviewScheduleController.java
@@ -28,9 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 /**
